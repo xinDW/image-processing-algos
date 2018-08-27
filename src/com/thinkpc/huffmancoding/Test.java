67a545b08732c44d41c5619f3384da41516616fd
xinDW <1320598835@qq.com>
2018-08-27 23:59:16 +0800

    minor fix

--- src/com/thinkpc/huffmancoding/Test.java
+++ src/com/thinkpc/huffmancoding/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		HuffmanCoding hc = new HuffmanCoding("1.Never in the history of calm down has anyone calmed down by been told to calm down");
 		String code = hc.encoding();
 		System.out.println(hc.decoding(code));
+		
 	}
 
 }
